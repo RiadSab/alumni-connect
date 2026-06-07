595c3920dc747691368bf236de6a9f47ce530744
RiadSab <sabti.ensa@uhp.ac.ma>
2026-06-07 14:43:35 +0100

    chore: initial project setup

---
+++ server/src/main/java/dev/sabti/alumni_connect/AlumniConnectApplication.java
@@ -0,0 +1,13 @@
+package dev.sabti.alumni_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniConnectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/sabti/alumni_connect/AlumniConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sabti.alumni_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
