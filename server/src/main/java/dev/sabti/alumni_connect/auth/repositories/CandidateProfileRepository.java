c06c8f006c1df64acbd7a4ad45708fb214ef7321
RiadSab <sabti.ensa@uhp.ac.ma>
2026-06-08 14:44:17 +0100

    feat(Candidate): add findByUser method to repo

--- server/src/main/java/dev/sabti/alumni_connect/auth/repositories/CandidateProfileRepository.java
+++ server/src/main/java/dev/sabti/alumni_connect/auth/repositories/CandidateProfileRepository.java
@@ -1,9 +1,13 @@
 package dev.sabti.alumni_connect.auth.repositories;
 
 import dev.sabti.alumni_connect.auth.entities.CandidateProfile;
+import dev.sabti.alumni_connect.auth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CandidateProfileRepository extends JpaRepository<CandidateProfile, Long> {
+    Optional<CandidateProfile> findByUser(User user);
 }
