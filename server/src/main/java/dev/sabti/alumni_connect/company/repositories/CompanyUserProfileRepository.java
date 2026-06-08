55b0ecd8d3ec3f259c2017eb83ffade142554429
RiadSab <sabti.ensa@uhp.ac.ma>
2026-06-08 13:36:14 +0100

    feat(company: )add findByUser method, return CompanyUserProfile

--- server/src/main/java/dev/sabti/alumni_connect/company/repositories/CompanyUserProfileRepository.java
+++ server/src/main/java/dev/sabti/alumni_connect/company/repositories/CompanyUserProfileRepository.java
@@ -1,9 +1,13 @@
 package dev.sabti.alumni_connect.company.repositories;
 
+import dev.sabti.alumni_connect.auth.entities.User;
 import dev.sabti.alumni_connect.company.entities.CompanyUserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyUserProfileRepository extends JpaRepository<CompanyUserProfile, Long> {
+    Optional<CompanyUserProfile> findByUser(User user);
 }
